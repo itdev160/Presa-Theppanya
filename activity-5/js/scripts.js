eb17db2e615b247a260b09fefcda8ad691f338bb
Presaurusrex112 <theppanp@gmatc.matc.edu>
2020-10-13 16:45:55 -0500

    Completed Activity 5

--- activity-5/js/scripts.js
+++ activity-5/js/scripts.js
@@ -4,10 +4,10 @@
 (function() {
 
     /**************************************
-     * Package data and constructor objects
+     * Extension data and constructor objects
      **************************************/
 
-     // Package data array (simulated data source, such as JSON or database records
+     // Extension data array (simulated data source, such as JSON or database records
     var data = [
         {
             name: 'GitLens-Git supercharged',
@@ -41,7 +41,7 @@
         }
     ];
 
-    function Package(data) {
+    function Extension(data) {
         this.name = data.name;
         this.description = data.description;
         this.author = data.author;
@@ -75,11 +75,11 @@
         return document.getElementById(id);
     };
 
-    //Writes the package object data to the
+    //Writes the extension object data to the
     //appropriate DOM elements utilizing the package selector property.
-    var writePackageInfo = function (package) {
+    var writeExtensionInfo = function(extension) {
         //Get reference to DOM elements
-        var selector = package.selector,
+        var selector = extension.selector,
             nameEl = getEl(selector + '-name'),
             descEl = getEl(selector + '-description'),
             authEl = getEl(selector + '-author'),
@@ -87,27 +87,27 @@
             starsEl = getEl(selector + '-stars'),
             priceEl = getEl(selector + '-price');
     
-        //Write package to the DOM elements
-        nameEl.textContent = package.name;
-        descEl.textContent = package.description;
-        authEl.textContent = package.author;
-        downloadEl.textContent = package.getFormattedDownloads();
-        starsEl.textContent = package.getFormattedStars();
-        priceEl.textContent = package.price;
+        //Write Extension to the DOM elements
+        nameEl.textContent = extension.name;
+        descEl.textContent = extension.description;
+        authEl.textContent = extension.author;
+        downloadEl.textContent = extension.getFormattedDownloads();
+        starsEl.textContent = extension.getFormattedStars();
+        priceEl.textContent = extension.price;
     };
 
     //write date
     var dateEl = getEl('date');
     dateEl.textContent = getTodaysDate();
 
-    //write package data 1 by 1
-    var gitLens = new Package(data[0]);
-    writePackageInfo(gitLens);
+    //write Extension data 1 by 1
+    var gitLens = new Extension(data[0]);
+    writeExtensionInfo(gitLens);
 
-    var liquid = new Package(data[1]);
-    writePackageInfo(liquid);
+    var liquid = new Extension(data[1]);
+    writeExtensionInfo(liquid);
 
-    var settings = new Package(data[2]);
-    writePackageInfo(settings);
+    var settings = new Extension(data[2]);
+    writeExtensionInfo(settings);
 
 } ());
